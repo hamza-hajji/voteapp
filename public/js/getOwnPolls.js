fb1ceff63576f38763ee25ae99c50e0a2ead380a
Hamza Hajji <hmzhjji@gmail.com>
2017-10-16 21:53:26 +0100

    few modifications to views and form validation with html5

--- public/js/getOwnPolls.js
+++ public/js/getOwnPolls.js
@@ -3,17 +3,24 @@ $(document).ready(function () {
     url: `${window.location.origin}/api/mypolls`,
     method: 'GET',
     success: function (data) {
-      for (var poll of data.polls) {
-        var pollLink = '/polls/' + poll.name;
-        var $pollElement = `
-        <li class="list-group-item list-group-item-action flex-column align-items-start">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1"><a class="text-dark" href="${pollLink}" target="_blank">${poll.name}</a></h5><br><br>
-            <button data-value="${poll.name}" id="deletePoll" class="btn btn-danger">Delete</button>
-          </div>
-        </li>
-        `;
-        $('.list-group').append($pollElement);
+      if (data.polls.length === 0) {
+        $('#output').html(`
+          <p class="lead">You currently have no polls! wanna create one?</p>
+          <a href="/new" class="btn btn-dark">New Poll</a>
+        `);
+      } else {
+        for (var poll of data.polls) {
+          var pollLink = '/polls/' + poll.name;
+          var $pollElement = `
+          <li class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1"><a class="text-dark" href="${pollLink}" target="_blank">${poll.name}</a></h5><br><br>
+              <button data-value="${poll.name}" id="deletePoll" class="btn btn-danger">Delete</button>
+            </div>
+          </li>
+          `;
+          $('.list-group').append($pollElement);
+        }
       }
     }
   });
--- public/js/getPolls.js
+++ public/js/getPolls.js
@@ -3,17 +3,25 @@ $(document).ready(function () {
     url: `${window.location.origin}/api/polls`,
     method: 'GET',
     success: function (data) {
-      for (var poll of data.polls) {
-        var pollLink = '/polls/' + poll.name;
-        var $pollElement = `
-        <a href="${pollLink}" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${poll.name}</h5>
-            <small>${poll.options.length}</small>
-          </div>
-          <p class="mb-1">created by ${poll.user}</p>
-        </a>`;
-        $('.list-group').append($pollElement);
+
+      if (data.polls.length === 0) {
+        $('#output').html(`
+          <p class="lead">There are currently no polls! wanna create one?</p>
+          <a href="/new" class="btn btn-dark">New Poll</a>
+        `);
+      } else {
+        for (var poll of data.polls) {
+          var pollLink = '/polls/' + poll.name;
+          var $pollElement = `
+          <a href="${pollLink}" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">${poll.name}</h5>
+              <small>${poll.options.length}</small>
+            </div>
+            <p class="mb-1">created by ${poll.user}</p>
+          </a>`;
+          $('.list-group').append($pollElement);
+        }
       }
     }
   });
